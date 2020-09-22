6d401126bfd99e877e11d8463a31b1d8627d8ff4
gagocarrilloedgar <edgargc.upc@gmail.com>
2020-09-23 00:21:48 +0200

    blue loading

--- src/common/Loading/Loading.js
+++ src/common/Loading/Loading.js
@@ -5,13 +5,15 @@ import { css } from "@emotion/core";
 // Can be a string as well. Need to ensure each key-value pair ends with ;
 const override = css`
   margin: auto;
-  border-color: black;
+  border-color: #2255ff;
+  padding-top: 200px;
+
 `;
 
 export const Loading = () => {
   return (
     <div className="sweet-loading">
-      <ClipLoader css={override} size={30}  />
+      <ClipLoader color={"#2255ff"} css={override} size={30} />
     </div>
   );
 };
